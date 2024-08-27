07f3a55f19e9bdbfbd1618d3fa014f55ab9c7171
Rajveer2278 <158049949+Rajveer2278@users.noreply.github.com>
2024-08-27 07:13:24 -0700

    Add files via upload
    Iris Dataset

---
+++ deploy.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('saved_model.sav', 'rb'))
+
+@app.route('/')
+def home():
+    result = ''
+    return render_template('index.html', result=result)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    sepal_length = float(request.form['sepal_length'])
+    sepal_width = float(request.form['sepal_width'])
+    petal_length = float(request.form['petal_length'])
+    petal_width = float(request.form['petal_width'])
+    
+    print(f"Inputs: {sepal_length}, {sepal_width}, {petal_length}, {petal_width}")
+    
+    result = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])[0]
+    
+    print(f"Prediction: {result}")
+    
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
